e1ed735b22bfc6bfced64258438a165c19d461f1
Martin Winkler <martin.winkler@meduniwien.ac.at>
2016-12-15 15:32:26 +0100

    staggered work

--- website/events/urls.py
+++ website/events/urls.py
@@ -1,15 +1,12 @@
 from django.conf.urls import url
 from randomizer import views
 from .views import StaggeredStartCreator, StaggeredStartRaceDetail, \
-    participants_list, StaggeredStartRaceInvite, enlist, \
-    StaggeredStartRaceStatus
+    participants_list, enlist, StaggeredStartRaceStatus
 
 urlpatterns = [
     url(r'^s/$', StaggeredStartCreator.as_view(), name='sscreator'),
     url(r'^s/(?P<pk>\d+)/$', StaggeredStartRaceDetail.as_view(),
         name='staggeredstartrace_detail'),
-    url(r'^s/(?P<pk>\d+)/inv/$', StaggeredStartRaceInvite.as_view(),
-        name='staggeredstartrace_invite'),
     url(r'^s/(?P<pk>\d+)/participantstable.json$',
         participants_list,
         name='participantslist'),
--- website/events/views.py
+++ website/events/views.py
@@ -2,12 +2,15 @@ import random
 import datetime
 
 from django.conf import settings
+from django.contrib import messages
 from django import forms
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.http import JsonResponse
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.shortcuts import get_object_or_404
+from django.views.decorators.cache import never_cache
 from django.views.generic import CreateView, DetailView
 
 from tracks.models import Laptime
@@ -34,11 +37,8 @@ class StaggeredStartCreator(CreateView):
         return super(StaggeredStartCreator, self).form_valid(form)
 
 
-class StaggeredStartRaceInvite(DetailView):
-    model = StaggeredStartRace
-
-
 @method_decorator(login_required, name='dispatch')
+@method_decorator(never_cache, name='dispatch')
 class StaggeredStartRaceDetail(DetailView):
     model = StaggeredStartRace
 
@@ -55,6 +55,31 @@ class StaggeredStartRaceDetail(DetailView):
         return context
 
     def post(self, *a, **k):
+        if self.request.POST.get('personal_laptime'):
+            timestr = self.request.POST.get('personal_laptime')
+            minu, seco = timestr.split(':')
+            millis = int(minu) * 60 * 1000 + float(seco) * 1000
+            event = self.get_object()
+            particip = SSRParticipation.objects.get(
+                staggeredstartrace=event,
+                player__username=self.request.user.username)
+            lt = Laptime(
+                track=event.track,
+                player=self.request.user,
+                recorded=datetime.date.today(),
+                vehicle=particip.vehicle,
+                untuned=particip.untuned,
+                millis=millis,
+                millis_per_km=millis / event.track.route_length_km,
+                comment='participation in a staggered start race',
+            )
+            lt.save()
+            particip.laptime = lt
+            particip.save()
+            messages.add_message(self.request, messages.SUCCESS,
+                                 'Thanks! (a nicer page will follow after the beta')
+            return HttpResponseRedirect('/')
+
         if self.request.POST.get('start_in_secs'):
             nowplus = random.randrange(0,6)
             nowplus += int(self.request.POST.get('start_in_secs'))
@@ -77,6 +102,8 @@ class StaggeredStartRaceDetail(DetailView):
             self.calculate_players_start_timestamps()
 
         return super(StaggeredStartRaceDetail, self).get(*a, **k)
+
+
 
     def calculate_players_start_timestamps(self):
         parts = list(self.object.ssrparticipation_set.all().order_by(
