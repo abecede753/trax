84cc38285e6bb288949ae48b8d3cd8d513d55a90
Martin Winkler <martin.winkler@meduniwien.ac.at>
2017-03-19 18:12:16 +0100

    more laptimes considered for player stats

--- website/players/utils.py
+++ website/players/utils.py
@@ -9,7 +9,7 @@ def update_player_racing_stats(player, commit=True):
     laptimes = list(Laptime.objects.filter(
         player=player).select_related(
         'vehicle').order_by('-recorded', '-created'))[:15]
-    if not laptimes:
+    if len(laptimes) < 8:
         player.defaultspeedmultiplier = 1.0
     else:
         millpkm_user = 0
